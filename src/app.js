49d228e22641aae0715f498aeb50c54f874f8fcb
Maël Donnart <57325246+maeldonn@users.noreply.github.com>
2020-09-30 11:51:16 +0200

    :sparkles: Client is now rendered by server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -8,8 +9,15 @@ const middlewares = require('./middlewares');
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(express.static(path.resolve('client/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('client/dist/index.html'));
+});
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
